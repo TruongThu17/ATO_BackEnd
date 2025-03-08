359c51d5b23fb8c3a3fcdfdfb74903bd603e632a
TruongThu17 <thutthe153251@fpt.edu.vn>
2025-03-08 15:17:57 +0700

    Update api upload img

--- ATO_Backend/ATO_API/Controllers/Files/FileController.cs
+++ ATO_Backend/ATO_API/Controllers/Files/FileController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ATO_API.Controllers.Files
 {
@@ -6,16 +8,23 @@ namespace ATO_API.Controllers.Files
     [ApiController]
     public class FileController : ControllerBase
     {
-        // Folder where files will be uploaded (can be adjusted)
         private readonly string _uploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
 
+        private readonly string _bucketName = "ato-c80b3.firebasestorage.app";
+        private readonly StorageClient _storageClient;
         public FileController()
         {
-            // Ensure the upload folder exists
             if (!Directory.Exists(_uploadFolder))
             {
                 Directory.CreateDirectory(_uploadFolder);
             }
+
+            string basePath = AppContext.BaseDirectory;
+
+            string credentialPath = Path.Combine(basePath, "Auth", "ato-c80b3-firebase-adminsdk-fbsvc-e6a97098c2.json");
+
+            GoogleCredential credential = GoogleCredential.FromFile(credentialPath);
+            _storageClient = StorageClient.Create(credential);
         }
 
         // POST api/file/upload
@@ -39,6 +48,24 @@ namespace ATO_API.Controllers.Files
 
             // Return the link to the uploaded file
             var fileUrl = $"/uploads/{uniqueFileName}";
+            return Ok(new { success = true, fileUrl });
+        }
+        [HttpPost("upload-image")]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("Không có file nào được upload.");
+            }
+
+            string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName).ToLower();
+
+            using (var stream = file.OpenReadStream())
+            {
+                await _storageClient.UploadObjectAsync(_bucketName, uniqueFileName, file.ContentType, stream);
+            }
+
+            var fileUrl = $"https://firebasestorage.googleapis.com/v0/b/{_bucketName}/o/{Uri.EscapeDataString(uniqueFileName)}?alt=media";
             return Ok(new { success = true, fileUrl });
         }
     }
