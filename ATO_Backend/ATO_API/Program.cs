778cc1d271f429560b1ff6823381ba381fcf4f21
TruongThu17 <thutthe153251@fpt.edu.vn>
2025-04-15 23:17:23 +0700

    .

--- ATO_Backend/ATO_API/Program.cs
+++ ATO_Backend/ATO_API/Program.cs
@@ -216,7 +216,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.ApplyMigrations();
+    //app.ApplyMigrations();
 }
 app.UseSwagger();
 app.UseSwaggerUI();
